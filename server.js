ac19270ad0bb329004b6a64bba427ffdc177d96a
Abdelkarim Essalim <karim.essalim@gmail.com>
2020-09-29 06:53:37 +0300

    finished the rest of the tutorial

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const flash = require("express-flash");
 const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
+const methodOverride = require("method-override");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
@@ -20,6 +21,7 @@ const users = [];
 
 app.set("view-engine", "ejs");
 
+//app.use
 app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(
@@ -32,6 +34,7 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(methodOverride("_method"));
 
 app.get("/", checkAuthenticated, (req, res) => {
   res.render("index.ejs", { name: req.user.name });
@@ -75,14 +78,18 @@ function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect("/login");
+  return res.redirect("/login");
 }
 
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    res.redirect("/");
+    return res.redirect("/");
   }
   next();
 }
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
 
 app.listen(3000);
